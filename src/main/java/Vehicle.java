81fac386b0b0ae2711d59dc2c667fad485cb98b9
Anigionn <arnogarrigue@gmail.com>
2020-05-12 22:50:11 +0200

    included a vulnerable old maven plugin

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -2,6 +2,12 @@ public class Vehicle {
      public int number;
      public String color;
 
+    /**
+     * Create a vehicle
+     *
+     * @param number : the number reference for that vehicle
+     * @param color : the color of the vehicle
+     */
     public Vehicle(int number, String color) {
         this.number = number;
         this.color = color;
